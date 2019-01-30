7a10b6c97051a6e5407e3636c1035759b476bc24
zbralex <5998971@gmail.com>
2019-01-30 20:27:26 +0300

    created new component with array

--- react-ui/src/components/Main.jsx
+++ react-ui/src/components/Main.jsx
@@ -3,7 +3,6 @@ import React, {Component} from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import articles from '../Fixtures'
-import ArticleList from './days/ArticleList'
 
 import D1 from './days/D1'
 import D2 from './days/D2'
@@ -31,8 +30,8 @@ class Main extends Component {
                         <D4 />
                         <D5 header="Day 5" text={articles[1].text} />
                         <D6 title={articles[5].title}  text={articles[5].text}/>
-                        <D7 title={articles[1].title} />
-                        <D8 title={articles[1].title} />
+                        <D7 />
+                        <D8  />
                     </section>
                     <Footer />
                 </div>
--- react-ui/src/components/days/ArticleList.jsx
+++ react-ui/src/components/days/ArticleList.jsx
@@ -3,12 +3,12 @@ import Article from '../Article'
 
 export default function ArticleList ({articles}) {
     const ArticleElements = articles.map(article => 
-        <li><Article article={article}/></li>
+       <Article article={article}/>
     )
     return (
-        <ul>
-            {ArticleElements}
-        </ul>
+        
+            <p>{ArticleElements}</p>
+        
     )
 
 }
