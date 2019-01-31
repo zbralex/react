abf06756770efe1529c5b46ac198d73c4a8e49b6
zbralex <5998971@gmail.com>
2019-01-31 20:11:23 +0300

    some styles for articles

---
+++ react-ui/src/components/days/D9.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+
+class D7 extends Component {
+    state = {
+        isOpen: true
+    }
+    render() {
+        const hiddenText = this.state.isOpen && <p className="card-text">{this.props.text}</p>
+        return(
+            <div className="card">
+                <div className="card-header">
+                    <h2 className="h2">{this.props.title}</h2>
+                </div>
+                <div className="card-body">
+                    {hiddenText}
+                    <button className="btn btn-primary" onClick={this.clickHandler}>{this.state.isOpen ? 'Close' : 'Open'}</button>
+                </div>
+            </div>
+        )
+    }
+    clickHandler = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+D7.defaultProps = {title: 'some title', text: 'some text'}
+export default D7
