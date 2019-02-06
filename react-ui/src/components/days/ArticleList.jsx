dd103e8c2a7ecf4202919599d48d8554577ab444
zbralex <5998971@gmail.com>
2019-02-06 16:32:46 +0300

    tryed constructor

--- react-ui/src/components/days/ArticleList.jsx
+++ react-ui/src/components/days/ArticleList.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Article from '../Article'
 
 export default function ArticleList ({articles}) {
-    const ArticleElements = articles.map(article => 
-       <Article article={article}/>
+    const ArticleElements = articles.map((article, index) => 
+       <Article article={article} defaultOpen = {index===1}/>
     )
     return (
         <p>{ArticleElements}</p>
